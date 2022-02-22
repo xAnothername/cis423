6980a901bb334c1c28a969c605483c16d2170223
xAnothername <65841804+xAnothername@users.noreply.github.com>
2022-02-22 14:38:04 -0800

    Update library.py

--- library.py
+++ library.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
 from sklearn.impute import KNNImputer
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_score
 from sklearn.experimental import enable_halving_search_cv
 from sklearn.model_selection import train_test_split, HalvingGridSearchCV
 from sklearn.linear_model import LogisticRegressionCV
